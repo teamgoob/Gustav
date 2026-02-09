27a252d63e83baad7827c7bd0d2392e0537a46d0
Kevin <choims1379@gmail.com>
2026-02-09 14:53:18 +0900

    Initial Commit

---
+++ Gustav/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gustav
+//
+//  Created by 최명수 on 2026/2/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
