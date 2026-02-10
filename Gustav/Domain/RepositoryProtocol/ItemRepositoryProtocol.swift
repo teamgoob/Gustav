ae693fd7e3c1c630d7f095158f113023d82b95d8
PSL0313 <sullin03@kakao.com>
2026-02-10 19:01:41 +0900

    P0 Fix: ItemRepositoryProtocol 요구사항 수정
    Async 메서드로 변경

--- Gustav/Domain/RepositoryProtocol/ItemRepositoryProtocol.swift
+++ Gustav/Domain/RepositoryProtocol/ItemRepositoryProtocol.swift
@@ -10,20 +10,20 @@ import Foundation
 // MARK: - 아이템 Repository Protocol
 protocol ItemRepositoryProtocol {
     // 워크스페이스 내 전체 아이템 목록 조회 (기본)
-    func fetchItems(workspaceId: UUID) -> RepositoryResult<[Item]>
+    func fetchItems(workspaceId: UUID) async -> RepositoryResult<[Item]>
 
     // 워크스페이스 내 조건 기반 아이템 조회
-    func queryItems(workspaceId: UUID, query: ItemQuery) -> RepositoryResult<[Item]>
+    func queryItems(workspaceId: UUID, query: ItemQuery) async -> RepositoryResult<[Item]>
 
     // 아이템 생성
-    func createItem(workspaceId: UUID, item: Item) -> RepositoryResult<Item>
+    func createItem(workspaceId: UUID, item: Item) async -> RepositoryResult<Item>
 
     // 아이템 수정
-    func updateItem(id: UUID, item: Item) -> RepositoryResult<Void>
+    func updateItem(id: UUID, item: Item) async -> RepositoryResult<Void>
 
     // 아이템 삭제
-    func deleteItem(id: UUID) -> RepositoryResult<Void>
+    func deleteItem(id: UUID) async -> RepositoryResult<Void>
 
     // 아이템 순서 변경 (indexKey)
-    func reorderItems(workspaceId: UUID, order: [UUID]) -> RepositoryResult<Void>
+    func reorderItems(workspaceId: UUID, order: [UUID]) async -> RepositoryResult<Void>
 }
