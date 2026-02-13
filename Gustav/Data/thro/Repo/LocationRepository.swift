bb427873de825129e0a733e9a44af1ef14d0ca0c
eunnim <251932852+SSong-iOSdev@users.noreply.github.com>
2026-02-13 12:10:53 +0900

    S 007 fix : 에러 주석처리

--- Gustav/Data/thro/Repo/LocationRepository.swift
+++ Gustav/Data/thro/Repo/LocationRepository.swift
@@ -4,38 +4,38 @@
 //
 //  Created by 박선린 on 2/12/26.
 //
-import Foundation
-
-final class LocationRepository: LocationRepositoryProtocol {
-    private let dataSource: LocationDataSourceProtocol
-    
-    init(dataSource remote: LocationDataSourceProtocol) {
-        self.dataSource = remote
-    }
-    
-    func fetchLocations(workspaceId: UUID) async -> RepositoryResult<[Location]> {
-        <#code#>
-    }
-    
-    func fetchLocation(id: UUID) async -> RepositoryResult<Location> {
-        <#code#>
-    }
-    
-    func createLocation(workspaceId: UUID, name: String, color: TagColor) async -> RepositoryResult<Location> {
-        <#code#>
-    }
-    
-    func updateLocation(id: UUID, location: Location) async -> RepositoryResult<Void> {
-        <#code#>
-    }
-    
-    func deleteLocation(id: UUID) async -> RepositoryResult<Void> {
-        <#code#>
-    }
-    
-    func reorderLocations(workspaceId: UUID, order: [UUID]) async -> RepositoryResult<Void> {
-        <#code#>
-    }
-    
-    
-}
+//import Foundation
+//
+//final class LocationRepository: LocationRepositoryProtocol {
+//    private let dataSource: LocationDataSourceProtocol
+//    
+//    init(dataSource remote: LocationDataSourceProtocol) {
+//        self.dataSource = remote
+//    }
+//    
+//    func fetchLocations(workspaceId: UUID) async -> RepositoryResult<[Location]> {
+//        <#code#>
+//    }
+//    
+//    func fetchLocation(id: UUID) async -> RepositoryResult<Location> {
+//        <#code#>
+//    }
+//    
+//    func createLocation(workspaceId: UUID, name: String, color: TagColor) async -> RepositoryResult<Location> {
+//        <#code#>
+//    }
+//    
+//    func updateLocation(id: UUID, location: Location) async -> RepositoryResult<Void> {
+//        <#code#>
+//    }
+//    
+//    func deleteLocation(id: UUID) async -> RepositoryResult<Void> {
+//        <#code#>
+//    }
+//    
+//    func reorderLocations(workspaceId: UUID, order: [UUID]) async -> RepositoryResult<Void> {
+//        <#code#>
+//    }
+//    
+//    
+//}
--- Gustav/Data/thro/Repo/ViewPresetRepository.swift
+++ Gustav/Data/thro/Repo/ViewPresetRepository.swift
@@ -4,29 +4,29 @@
 //
 //  Created by 박선린 on 2/12/26.
 //
-import Foundation
-
-final class ViewPresetRepository: ViewPresetRepositoryProtocol {
-    
-    private let dataSource: ViewPresetDataSourceProtocol 
-    
-    init(dataSource remote: ViewPresetDataSourceProtocol) {
-        self.dataSource = remote
-    }
-    
-    func fetchViewPresets(workspaceId: UUID) async -> RepositoryResult<[ViewPreset]> {
-        <#code#>
-    }
-    
-    func createViewPreset(workspaceId: UUID, preset: ViewPreset) async -> RepositoryResult<ViewPreset> {
-        <#code#>
-    }
-    
-    func updateViewPreset(id: UUID, preset: ViewPreset) async -> RepositoryResult<Void> {
-        <#code#>
-    }
-    
-    func deleteViewPreset(id: UUID) async -> RepositoryResult<Void> {
-        <#code#>
-    }
-}
+//import Foundation
+//
+//final class ViewPresetRepository: ViewPresetRepositoryProtocol {
+//    
+//    private let dataSource: ViewPresetDataSourceProtocol 
+//    
+//    init(dataSource remote: ViewPresetDataSourceProtocol) {
+//        self.dataSource = remote
+//    }
+//    
+//    func fetchViewPresets(workspaceId: UUID) async -> RepositoryResult<[ViewPreset]> {
+//        <#code#>
+//    }
+//    
+//    func createViewPreset(workspaceId: UUID, preset: ViewPreset) async -> RepositoryResult<ViewPreset> {
+//        <#code#>
+//    }
+//    
+//    func updateViewPreset(id: UUID, preset: ViewPreset) async -> RepositoryResult<Void> {
+//        <#code#>
+//    }
+//    
+//    func deleteViewPreset(id: UUID) async -> RepositoryResult<Void> {
+//        <#code#>
+//    }
+//}
