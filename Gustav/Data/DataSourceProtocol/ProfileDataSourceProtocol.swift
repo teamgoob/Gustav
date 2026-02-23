d595a54d8aba193c15338ac05779b317392bad27
eunnim <251932852+SSong-iOSdev@users.noreply.github.com>
2026-02-23 10:27:50 +0900

    S 018 fix : profile 인자 오류 수정

--- Gustav/Data/DataSourceProtocol/ProfileDataSourceProtocol.swift
+++ Gustav/Data/DataSourceProtocol/ProfileDataSourceProtocol.swift
@@ -11,11 +11,6 @@ protocol ProfileDataSourceProtocol {
     func fetchProfile(userId: UUID) async -> RepositoryResult<ProfileRecord>
     func updateUserName(userId: UUID, name: String) async -> RepositoryResult<Void>
 
-    
-//    func upsertEmailIfEmpty(userId: String, email: String) async -> RepositoryResult<Void>
-//    func upsertNameIfEmpty(userId: String, name: String) async -> RepositoryResult<Void>
-// 위 코드는 제거. 아래 코드로 대체.
-    
     // true: 신규 생성, false: 기존
     func bootstrapAfterAppleAuth(
         userId: UUID,
