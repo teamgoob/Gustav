a0f772aeed133172e7e4e981b7742b7fc755b135
Kevin <choims1379@gmail.com>
2026-03-03 23:03:09 +0900

    K21 Features: ColorSet / 프로젝트 상수 추가
    1. Assets에 프로젝트 ColorSet 등록
    2. Presentation/Convenience -> 색상 / 글꼴 / 아이콘 등 프로젝트 상수 파일 추가

---
+++ Gustav/Presentation/Convenience/Fonts.swift
@@ -0,0 +1,26 @@
+//
+//  Fonts.swift
+//  Gustav
+//
+//  Created by 최명수 on 2026/3/3.
+//
+
+import UIKit
+
+// MARK: - 앱 폰트
+struct Fonts {
+    
+    // Large Title
+    static let largeTitle: UIFont = .systemFont(ofSize: 34, weight: .bold)
+    
+    // Headline
+    static let headline: UIFont = .systemFont(ofSize: 17, weight: .semibold)
+    
+    // Body
+    static let body: UIFont = .systemFont(ofSize: 15, weight: .regular)
+    
+    // Caption
+    static let caption: UIFont = .systemFont(ofSize: 10, weight: .regular)
+    
+    private init() {}
+}
