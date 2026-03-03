f47b29142b94fc8a826a939abd99b20ff83dcc24
Kevin <choims1379@gmail.com>
2026-03-03 21:56:39 +0900

    K20 Features: SnapKit 라이브러리 추가, Main 스토리보드 제거
    1. SDK 활용하여 SnapKit 라이브러리 추가
    2. Code base UI 구현을 위해, Main 스토리보드 제거 및 프로젝트 설정 변경

--- Gustav/APP/SceneDelegate.swift
+++ Gustav/APP/SceneDelegate.swift
@@ -16,7 +16,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // UIWindow 생성
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        
+        // Root ViewController 생성
+        let rootViewController = ViewController()
+        
+        // Navigation Controller 생성
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        
+        // 루트 설정
+        window.rootViewController = navigationController
+        
+        // 화면 표시
+        self.window = window
+        window.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
