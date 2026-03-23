b82065496c58a35330d89110bd0b877fff2648ad
PSL0313 <sullin03@kakao.com>
2026-03-24 05:24:41 +0900

    P55 Feat: ItemAttribute(Category, State, Location TableView 사용)

---
+++ Gustav/Presentation/View/WorkspaceSetting/ItemAttributeBasicCell.swift
@@ -0,0 +1,98 @@
+//
+//  WorkSpaceTableViewBasicCell.swift
+//  Gustav
+//
+//  Created by 박선린 on 3/1/26.
+//
+
+import UIKit
+import SnapKit
+
+class ItemAttributeBasicCell: UITableViewCell {
+    static let reuseID = "ItemAttributeBasicCell"
+    
+    private let spacing: CGFloat = 16
+    
+    // MARK: - UI
+    
+    private let circlefill: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "circle.fill")
+        iv.tintColor = .red
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let l = UILabel()
+        l.font = .systemFont(ofSize: 16, weight: .semibold)
+        l.textColor = Colors.Text.main
+        l.textAlignment = .left
+        l.numberOfLines = 1
+        return l
+    }()
+    
+    private let chevronImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "chevron.right")
+        iv.tintColor = Colors.Text.main
+        return iv
+    }()
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupUI() {
+        selectionStyle = .none
+        backgroundColor = Colors.Theme.cardBackground
+        contentView.backgroundColor = Colors.Theme.cardBackground
+    }
+    
+    private func setupLayout() {
+        self.contentView.addSubview(circlefill)
+        self.contentView.addSubview(titleLabel)
+        self.contentView.addSubview(chevronImageView)
+        
+        circlefill.snp.makeConstraints {
+            $0.height.width.equalTo(20)
+            $0.leading.equalTo(self.contentView).offset(spacing)
+            $0.centerY.equalTo(self.contentView)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.height.equalTo(48)
+            $0.leading.equalTo(self.circlefill.snp.trailing).offset(spacing)
+            $0.trailing.equalTo(self.chevronImageView.snp.leading).offset(-spacing)
+            $0.centerY.equalTo(self.contentView)
+        }
+        
+        chevronImageView.snp.makeConstraints {
+            $0.trailing.equalTo(self.contentView).offset(-spacing)
+            $0.centerY.equalTo(self.contentView)
+        }
+    }
+    
+    // MARK: - Configure
+    
+    func configure(title: String, tagColor: TagColor) {
+        titleLabel.text = title
+        circlefill.tintColor = tagColor.uiColor
+    }
+    
+    // 재사용 이슈 방지
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+    }
+    
+}
---
+++ Gustav/Presentation/View/WorkspaceSetting/ItemAttributeReorderingCell.swift
@@ -0,0 +1,88 @@
+//
+//  WorkSpaceReorderingCell.swift
+//  Gustav
+//
+//  Created by 박선린 on 3/10/26.
+//
+
+import UIKit
+import SnapKit
+
+class ItemAttributeReorderingCell: UITableViewCell {
+
+    static let reuseID = "ItemAttributeReorderCell"
+    
+    private let spacing: CGFloat = 16
+    
+    // MARK: - UI
+    
+    private let circlefill: UIImageView = {
+        let iv = UIImageView()
+        iv.image = UIImage(systemName: "circle.fill")
+        iv.tintColor = .red
+        return iv
+    }()
+    
+    private let titleLabel: UILabel = {
+        let l = UILabel()
+        l.font = .systemFont(ofSize: 16, weight: .semibold)
+        l.textColor = Colors.Text.main
+        l.textAlignment = .left
+        l.numberOfLines = 1
+        return l
+    }()
+    
+    // MARK: - Init
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+        setupLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Setup
+    
+    private func setupUI() {
+        selectionStyle = .none
+        backgroundColor = Colors.Theme.cardBackground
+        contentView.backgroundColor = Colors.Theme.cardBackground
+        self.contentView.clipsToBounds = false
+        self.clipsToBounds = false
+    }
+    
+    private func setupLayout() {
+        self.contentView.addSubview(circlefill)
+        self.contentView.addSubview(titleLabel)
+        
+        circlefill.snp.makeConstraints {
+            $0.height.width.equalTo(20)
+            $0.leading.equalTo(self.contentView).offset(spacing)
+            $0.centerY.equalTo(self.contentView)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.height.equalTo(48)
+            $0.leading.equalTo(self.circlefill.snp.trailing).offset(spacing)
+            $0.trailing.equalTo(self.contentView.snp.trailing).offset(-spacing)
+            $0.centerY.equalTo(self.contentView)
+        }
+        
+    }
+    
+    // MARK: - Configure
+    
+    func configure(title: String, tagColor: TagColor) {
+        titleLabel.text = title
+        circlefill.tintColor = tagColor.uiColor
+    }
+    
+    // 재사용 이슈 방지
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+    }
+}
